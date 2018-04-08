ff72846022940167844ca7f026a2a762375dc4c5
Yana Pertels <ypertels@gmail.com>
2018-04-08 00:44:10 -0700

    walking animation; constant walking speed; animated nav pointer

--- Single In Your Area/Scripts/NavPoint.cs
+++ Single In Your Area/Scripts/NavPoint.cs
@@ -6,19 +6,42 @@ public class NavPoint : Node2D
     // Member variables here, example:
     // private int a = 2;
     // private string b = "textvar";
+	public Vector2 clickPosition;
+	bool goingUp = true;
+	float stepSize = 0.25f;
+	int cycleLength = 500;
+	int timeLastUpdate = 0;
 	
     public override void _Ready()
     {
         // Called every time the node is added to the scene.
         // Initialization here
-     
+
     }
 
    public override void _Process(float delta)
    {
+		MovePointer(); 
+		
         if (Input.IsActionPressed("left_click")) {
-            this.SetPosition(GetGlobalMousePosition());
+			
+			clickPosition = GetGlobalMousePosition();
+            this.SetPosition(clickPosition);
+			
         // MoveAndSlide(GetGlobalMousePosition() - GetPosition());
 		}       
    }
+	
+   public void MovePointer() {
+	
+	    if (OS.GetTicksMsec() - timeLastUpdate > cycleLength) {
+			goingUp = !goingUp;
+			timeLastUpdate = OS.GetTicksMsec();
+		}
+		if (goingUp) {
+			this.SetPosition(this.GetPosition() + new Vector2(0f, stepSize));
+		} else {
+			this.SetPosition(this.GetPosition() - new Vector2(0f, stepSize));
+		}
+	}
 }
--- Single In Your Area/Scripts/Player.cs
+++ Single In Your Area/Scripts/Player.cs
@@ -3,32 +3,71 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    // Member variables here, example:
-    // private int a = 2;
-    // private string b = "textvar";
-	const int walk_speed = 200;
-    Vector2 velocity;
+
+    const float walkSpeed = 100f;
+	public bool isMoving = false;
+	int currentFrame = 0;
+	const int maxFrames = 4;
+	enum Direction: int {Left = 4, Right = 8, Up = 0, Down = 12}; // value is number of frame 
+	
+	const int timeBetweenUpdates = 100;
+	int timeLastUpdate = 0;
     
-    Node2D nav_pt;
+	Direction currentDirection = Direction.Up;
+	Sprite sprite;
+    NavPoint navPt;
+	Vector2 offset = new Vector2(10,30); // offset in order not to block the character by the nav arrow
 
     public override void _Ready()
     {
         GD.Print("HELLO THERE");
         // Called every time the node is added to the scene.
         // Initialization here
-        nav_pt = (Node2D)GetNode("../navpoint_container");
-        // Node2D s = (Node2D)GetNode("../navpoint_container");
-
+        navPt = (NavPoint)GetNode("../navpoint_container");
+        sprite = (Sprite)GetNode("Sprite");
         
     }
 
     public override void _Process(float delta)
     {
-
-		Vector2 target_pos = nav_pt.Position;
-		MoveAndSlide(target_pos - GetPosition());
-	//	if (Input.IsActionPressed("left_click")) {
-        	
-	//	}
+	
+		if ((OS.GetTicksMsec() - timeLastUpdate > timeBetweenUpdates) && isMoving) {
+			currentFrame = (currentFrame + 1) % maxFrames;
+			timeLastUpdate = OS.GetTicksMsec();
+		}
+		
+		sprite.Frame = (int) currentDirection + currentFrame;
+		
+		Vector2 targetPos = navPt.clickPosition + offset;
+		Vector2 movePosition = targetPos - GetPosition();
+		
+		if (movePosition.Length() <= 1) { // Comparing to 1 vs 0 to avoid weird jitter
+			movePosition = new Vector2(0,0);
+			isMoving = false;
+			currentFrame = 0;
+		} else {
+			isMoving = true;
+			UpdateDirection(movePosition);
+		}
+		
+        MoveAndSlide((movePosition.Normalized()*walkSpeed));
+		
     }
+	
+	private void UpdateDirection(Vector2 movePosition) {
+		if (Mathf.Abs(movePosition.x) > Mathf.Abs(movePosition.y)) {
+				if (movePosition.x > 0) {
+					currentDirection = Direction.Right;
+				} else {
+					currentDirection = Direction.Left;
+				}
+			} else {
+				if (movePosition.y > 0) {
+					currentDirection = Direction.Up;
+				} else {
+					currentDirection = Direction.Down;
+				}
+			}
+		
+	}
 }
