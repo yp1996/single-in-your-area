8207462293c46039e07337089070f5f50b56be10
dvdmrn <dgm.marino@gmail.com>
2018-04-07 19:04:31 -0700

    viapoint navigation

---
+++ Single In Your Area/Scripts/NavPointFade.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class NavPointFade : Sprite
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {
+                
+    }
+
+//    public override void _Process(float delta)
+//    {
+//        // Called every frame. Delta is time since last frame.
+//        // Update game logic here.
+//        
+//    }
+}
