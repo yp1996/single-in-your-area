3714099a018f8efa87aafcfa70f80bf81a07db56
Yana Pertels <ypertels@gmail.com>
2018-07-28 13:11:46 -0700

    swiping on findr

--- Single In Your Area/Scenes/GUI/newGame/tinderMenu.gd
+++ Single In Your Area/Scenes/GUI/newGame/tinderMenu.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends Container
 
 # class member variables go here, for example:
 # var a = 2
---
+++ Single In Your Area/Scripts/TinderMatch.cs
@@ -0,0 +1,155 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class TinderMatch : Node2D
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    private Sprite sprite;
+	private Sprite nextSprite;
+	private RichTextLabel label;
+	private Vector2 lastMousePosition; 
+	private Vector2 screenSize;
+	private Vector2 screenCentre;
+	private float slideSpeed = 10f;
+	
+	private float cooldownTime = 1f;
+	private float maxCooldown = 1f;
+	
+	public string scene = "res://Scenes/Levels/Bedroom.tscn";
+	
+	public struct TinderProfile {
+		public Texture picture;
+		public string bio;
+		
+		public TinderProfile(string path, string bioText) {
+			picture = (Texture) GD.Load("res://UI/findr/" + path + ".png");
+			bio = bioText;
+		}
+	}
+	
+	
+	private List<TinderProfile> tinderProfiles;
+	private TinderProfile currentProfile;
+	private TinderProfile nextProfile;
+	private int currentIndex = 0;
+
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+
+        sprite = (Sprite)GetNode("Pic");
+		nextSprite = (Sprite)GetNode("NextPic");
+		label = (RichTextLabel)GetNode("Bio");
+		screenSize = GetViewportRect().Size;
+		GD.Print(screenSize.x);
+		screenCentre = screenSize / 2f;
+		lastMousePosition = screenCentre;
+		tinderProfiles = new List<TinderProfile>();
+		tinderProfiles.Add(new TinderProfile("dead", "Not like the other girls. I'm dead"));
+		tinderProfiles.Add(new TinderProfile("onfire", "In She’ol but movin’ on up to the world to come"));
+		tinderProfiles.Add(new TinderProfile("fishing", "I beach long walks on the enjoy"));
+		tinderProfiles.Add(new TinderProfile("skeleycat", "Looking for the Dennett to my Chomsky. Enjoy well-read, thoughtful people and intellectual discussions. Sapiosexual."));
+		tinderProfiles.Add(new TinderProfile("spookyskeleton", "I’ve been dead since Wednesday lmao"));
+		tinderProfiles.Add(new TinderProfile("slime", "Not technically 'alive' (wednesday amirite?) but looking to meet new 'people'"));
+		tinderProfiles.Add(new TinderProfile("ina", "“Somehow survived” Wednesday, so ofc I’m on Findr"));
+    	currentProfile = tinderProfiles[currentIndex];
+		nextProfile = tinderProfiles[currentIndex + 1];
+		nextSprite.SetTexture(nextProfile.picture);
+		
+	}
+	
+	public override void _Process(float delta) {
+		
+		if (IsVisible()) {
+		
+		if (cooldownTime <= 0f) { // Block swiping so that you don't swipe through all profiles at once
+		
+			Vector2 currentPosition = sprite.GetPosition();
+			
+			if (Input.IsActionPressed("left_click")) {
+				
+				Vector2 clickPosition = GetGlobalMousePosition();
+	            float deltaClick = clickPosition.x - lastMousePosition.x;
+				sprite.SetPosition(new Vector2(clickPosition.x, sprite.GetPosition().y));
+				lastMousePosition = clickPosition;
+			}   
+			
+			else {
+				float distanceFromCentre = Mathf.Min(Mathf.Abs(currentPosition.x - screenCentre.x), slideSpeed);
+				currentPosition.x += distanceFromCentre * Mathf.Sign(screenCentre.x - currentPosition.x);
+				sprite.SetPosition(currentPosition);
+				nextSprite.SetPosition(new Vector2(screenSize.x - currentPosition.x, currentPosition.y));
+			}
+			
+			if (hasSwipedLeft(currentPosition)) {
+				currentIndex++;
+				if (currentIndex < tinderProfiles.Count) {
+					cooldownTime = maxCooldown;
+					currentProfile = tinderProfiles[currentIndex];
+					if (currentIndex < tinderProfiles.Count - 1) {
+						nextProfile = tinderProfiles[currentIndex + 1];
+						nextSprite.SetTexture(nextProfile.picture);
+					}
+					else {
+						nextSprite.SetTexture(null);
+					}
+					sprite.SetTexture(currentProfile.picture);
+					label.SetText(currentProfile.bio);
+					currentPosition.x = screenCentre.x;
+					sprite.SetPosition(currentPosition);
+					nextSprite.SetPosition(currentPosition);
+					
+				}
+			}
+			
+			if (hasSwipedRight(currentPosition)) {
+				
+				var global = (Global)GetNode("/root/Global");
+                global.GotoScene(scene);
+			}
+		}
+		
+		else {
+			cooldownTime -= delta;
+		}
+		}
+		  
+	}
+	
+	private bool hasSwipedLeft(Vector2 currentPosition) {
+		// For the first n-1 profiles you can only swipe left 
+		return (currentPosition.x <= 0f && currentIndex < tinderProfiles.Count - 1);
+	}
+	
+	private bool hasSwipedRight(Vector2 currentPosition) {
+		// For the first n-1 profiles you can only swipe left 
+		return (currentPosition.x >= screenSize.x && currentIndex >= tinderProfiles.Count - 1);
+	}
+	
+	private void OnLeftInputEvent(Godot.Object viewport, Godot.Object input, int shape_idx)
+	{
+		GD.Print("O look an event");
+	    if ((InputEvent) input is InputEventMouseButton button) {
+			sprite.Hide();
+		}
+	}
+	
+	private void OnRightInputEvent(System.Object viewport, System.Object @event, int shape_idx)
+	{
+	    // Replace with function body
+		GD.Print("O look an event right");
+	}
+
+
+
+}
+
+
+
+
+
